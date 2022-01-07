00f473c9e1271a18755294b1698d9e757db064a2
HyunG1u <shgusrb0402@naver.com>
2022-01-07 17:21:43 +0900

    feat: first NextJS

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import NavBar from "../components/NavBar";
 
-export default function Hmoe() {
+export default function potato() {
   return (
     <div>
       <NavBar />
-      <h1>Hello</h1>
+      <h1>About us</h1>
     </div>
   );
 }
